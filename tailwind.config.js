e1a4d4d1563f4f46dfc8e14f37c743d8ac259400
A2nurag <anurag2sarkar@gmail.com>
2021-12-03 22:12:04 +0530

    fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,13 @@
 module.exports = {
-  purge: [],
+  purge: ['./src/**/*.html', './src/**/*.js'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       fontFamily: {
-        'mont':['Montserrat','sans-serif'],
-        'ubu':['Ubuntu','sans-serif'],
-        'jos':['Josefin Sans','sans-serif']
-      }
+        mont: ['Montserrat', 'sans-serif'],
+        ubu: ['Ubuntu', 'sans-serif'],
+        jos: ['Josefin Sans', 'sans-serif'],
+      },
     },
   },
   variants: {
